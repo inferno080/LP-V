5695a0d13d59ddb73723cd0ce844eda28f392ad3
Yash Kale <inferno080.com@gmail.com>
2023-05-28 13:30:34 +0530

    Assignment ? Code Verified and Working Successfully

--- Client_Server_Java_Socket/Client.java
+++ Client_Server_Java_Socket/Client.java
@@ -1,3 +1,31 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
-    
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 12345);
+            System.out.println("Connected to server: " + socket.getInetAddress().getHostAddress());
+
+            // Create input and output streams for the server
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            // Send a message to the server
+            String message = "Hello, server!";
+            output.writeUTF(message);
+            System.out.println("Sent message to server: " + message);
+
+            // Receive and print the response from the server
+            String response = input.readUTF();
+            System.out.println("Received response from server: " + response);
+
+            // Clean up resources
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Client_Server_Java_Socket/Server.java
+++ Client_Server_Java_Socket/Server.java
@@ -1,4 +1,37 @@
-public class Server 
-{
-    
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(12345);
+            System.out.println("Server started. Waiting for client connections...");
+
+            Socket socket = serverSocket.accept();
+            System.out.println("Client connected: " + socket.getInetAddress().getHostAddress());
+
+            // Create input and output streams for the client
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            // Read client message
+            String message = input.readUTF();
+            System.out.println("Received message from client: " + message);
+
+            // Send response to the client
+            String response = "Server says: Message received!";
+            output.writeUTF(response);
+            System.out.println("Sent response to client: " + response);
+
+            // Clean up resources
+            socket.close();
+            serverSocket.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
