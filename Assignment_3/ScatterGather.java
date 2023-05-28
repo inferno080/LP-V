37449c3101de21f39bd962de9e7378b2e5e95277
Yash Kale <inferno080.com@gmail.com>
2023-05-28 15:09:40 +0530

    Assignment 3 Code Verified and Working Successfully

--- Assignment_3/ScatterGather.java
+++ Assignment_3/ScatterGather.java
@@ -1,75 +1,49 @@
-import mpi.MPI;
-import java.util.Scanner;
 import mpi.*;
 
 public class ScatterGather {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws MPIException {
         MPI.Init(args);
-        //  Get rank of each process and size of communicator
+
         int rank = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
-        
-        int unitsize = 5;
-        int root = 0;
-        int send_buffer[] = null;
-        //  1 process is expected to handle 4 elements
-        send_buffer = new int [unitsize * size];
-        int recieve_buffer[] = new int [unitsize];
-        int new_recieve_buffer[] = new int [size];
 
-        //  Set data for distribution
-        if(rank == root) {
-            int total_elements = unitsize * size;
-            System.out.println("Enter " + total_elements + " elements");
-            for(int i = 0; i < total_elements; i++) {
-                System.out.println("Element " + i + "\t = " + i);
-                send_buffer[i] = i;
-            }
+        // Define the array to be summed
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Replace with your own array
+
+        // Calculate the number of elements to be processed by each processor
+        int n = array.length;
+        int elementsPerProcessor = n / size;
+
+        // Allocate memory to store local elements
+        int[] localElements = new int[elementsPerProcessor];
+
+        // Scatter the array elements to all processors
+        MPI.COMM_WORLD.Scatter(array, 0, elementsPerProcessor, MPI.INT,
+                localElements, 0, elementsPerProcessor, MPI.INT, 0);
+
+        // Calculate the local sum
+        int localSum = 0;
+        for (int i = 0; i < elementsPerProcessor; i++) {
+            localSum += localElements[i];
         }
 
-        //  Scatter data to processes
-        MPI.COMM_WORLD.Scatter(
-            send_buffer,
-            0,
-            unitsize,
-            MPI.INT,
-            recieve_buffer,
-            0,
-            unitsize,
-            MPI.INT,
-            root
-        );
+        // Display the intermediate sums calculated at different processors
+        System.out.println("Processor " + rank + ": Local sum = " + localSum);
 
-        //  Calculate sum at non root processes
-        //  Store result in first index of array
-        for(int i = 1; i < unitsize; i++) {
-            recieve_buffer[0] += recieve_buffer[i];
-        }
-        System.out.println(
-            "Intermediate sum at process " + rank + " is " + recieve_buffer[0]
-        );
-        
+        // Allocate memory for the global sums at all processes
+        int[] globalSums = new int[size];
 
-        //  Gather data from processes
-        MPI.COMM_WORLD.Gather(
-            recieve_buffer,
-            0,
-            1,
-            MPI.INT,
-            new_recieve_buffer,
-            0,
-            1,
-            MPI.INT,
-            root
-        );
+        // Gather all local sums at all processes
+        MPI.COMM_WORLD.Gather(new int[]{localSum}, 0, 1, MPI.INT,
+                globalSums, 0, 1, MPI.INT, 0);
 
-        //  Aggregate output from all non root processes
-        if(rank == root) {
-            int total_sum = 0;
-            for(int i = 0; i < size; i++) {
-                total_sum += new_recieve_buffer[i];
+        // Display the final sum at processor 0
+        if (rank == 0) {
+            int finalSum = 0;
+            for (int sum : globalSums) {
+                finalSum += sum;
             }
-            System.out.println("Final sum : " + total_sum);
+            System.out.println("Final sum = " + finalSum);
         }
 
         MPI.Finalize();
